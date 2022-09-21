bef39f784cd01c91ae8313ee516f2cb055a231eb
nafiul1351 <nafiul1351@gmail.com>
2022-09-21 22:35:27 +0600

    second commit

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\Inputtest;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,14 +18,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 })->name('welcome');
-Route::post('/add', [App\Http\Controllers\Inputtest::class, 'add'])->name('add');
-Route::get('/view', [App\Http\Controllers\Inputtest::class, 'view'])->name('view');
-Route::get('/update/data/{id}', [App\Http\Controllers\Inputtest::class, 'update'])->name('update');
-Route::post('/update/data/{id}', [App\Http\Controllers\Inputtest::class, 'update2'])->name('update2');
-Route::get('/delete/data/{id}', [App\Http\Controllers\Inputtest::class, 'delete'])->name('delete');
-Route::get('/add_2', [App\Http\Controllers\Inputtest::class, 'add2'])->name('add2');
-Route::post('/add_2', [App\Http\Controllers\Inputtest::class, 'add3'])->name('add3');
-Route::get('/view_2', [App\Http\Controllers\Inputtest::class, 'view2'])->name('view2');
-Route::get('/update/data_2/{id}', [App\Http\Controllers\Inputtest::class, 'update3'])->name('update3');
-Route::post('/update/data_2/{id}', [App\Http\Controllers\Inputtest::class, 'update4'])->name('update4');
-Route::get('/delete/data_2/{id}', [App\Http\Controllers\Inputtest::class, 'delete2'])->name('delete2');
+Route::post('/add', [Inputtest::class, 'add'])->name('add');
+Route::get('/view', [Inputtest::class, 'view'])->name('view');
+Route::get('/update/data/{id}', [Inputtest::class, 'update'])->name('update');
+Route::post('/update/data/{id}', [Inputtest::class, 'update2'])->name('update2');
+Route::get('/delete/data/{id}', [Inputtest::class, 'delete'])->name('delete');
+Route::get('/add_2', [Inputtest::class, 'add2'])->name('add2');
+Route::post('/add_2', [Inputtest::class, 'add3'])->name('add3');
+Route::get('/view_2', [Inputtest::class, 'view2'])->name('view2');
+Route::get('/update/data_2/{id}', [Inputtest::class, 'update3'])->name('update3');
+Route::post('/update/data_2/{id}', [Inputtest::class, 'update4'])->name('update4');
+Route::get('/delete/data_2/{id}', [Inputtest::class, 'delete2'])->name('delete2');
